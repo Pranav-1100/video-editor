5a131e3e1f63cc6bb6d6061e30c2d6ddf494030f
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-08-12 22:44:28 +0530

    added basic files for video editor and added some controls for video editor

---
+++ video-editor/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoPlayer from './components/VideoPlayer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Video Flip Editor</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
+export default App;
