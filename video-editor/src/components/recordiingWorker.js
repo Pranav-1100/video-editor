0da852668d57401250455336d7f3a6a552eb22dc
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-08-13 09:13:12 +0530

    added some changes for cropper and videoplayer and some changes in recording worker

--- video-editor/src/components/recordiingWorker.js
+++ video-editor/src/components/recordiingWorker.js
@@ -1,8 +1,8 @@
 let recordedData = [];
 
-// eslint-disable-next-line no-restricted-globals
 self.onmessage = function(e) {
   if (e.data.type === 'record') {
+    console.log('Recording data:', event.data.data);
     recordedData.push(e.data.data);
   } else if (e.data.type === 'download') {
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(recordedData, null, 2));
